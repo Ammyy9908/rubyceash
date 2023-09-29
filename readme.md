6d27af307b348830cc3d9e146c90dd52fff80673
kumarsumitkodnest <sumit@kodnest.com>
2023-09-30 00:43:06 +0530

    syntax added in week-1

---
+++ readme.md
@@ -0,0 +1,61 @@
+
+# Ruby Programming Course
+Welcome to the comprehensive Ruby Programming Course! This course is designed for beginners to advanced developers looking to hone their skills in the Ruby language.
+## Table of Contents
+- [Course Overview](#course-overview)
+- [Installation and Setup](#installation-and-setup)
+- [Modules](#modules)
+    - [Ruby Basics](#ruby-basics)
+    - [Backend Development Concepts](#backend-development-concepts)
+    - [Ruby on Rails Basics](#ruby-on-rails-basics)
+    - [Advanced Topics & Projects](#advanced-topics--projects)
+- [Resources](#resources)
+- [Contributors](#contributors)
+- [License](#license)
+
+## Course Overview
+
+This course covers everything from the foundational concepts of the Ruby language to the advanced techniques used in professional software development. By the end of the course, you'll be confident in building robust and efficient web applications using Ruby and the Ruby on Rails framework.
+
+## Installation and Setup
+
+Before starting the course, ensure you have Ruby and the necessary tools installed. Follow our [Installation Guide](installation.md) for step-by-step instructions.
+
+## Modules
+
+### Ruby Basics
+- Introduction to the Ruby language
+- Variables, Data Types, and Control Structures
+- Methods, Classes, and Object-Oriented Programming
+- Advanced topics: Blocks, Procs, Lambdas, and Modules
+
+### Backend Development Concepts
+- Understanding HTTP and REST principles
+- Introduction to Sinatra and basic web application development
+- Databases, SQL basics, and CRUD operations
+- Integrating databases with Sinatra
+
+### Ruby on Rails Basics
+- Setting up Ruby on Rails and understanding MVC
+- Dive into ActiveRecord, Models, Migrations, and Associations
+- Routes, Controllers, Views, and Layouts
+- Advanced Rails: Forms, Validations, Sessions, Cookies, and Authentication
+
+### Advanced Topics & Projects
+- Advanced ActiveRecord techniques and building APIs in Rails
+- Asynchronous Tasks, Mailers, File Uploads, Pagination, and Searching
+- Two-week capstone project to consolidate learning
+
+## Resources
+
+- [Official Ruby Documentation](https://ruby-doc.org/)
+- [Ruby on Rails Guides](https://guides.rubyonrails.org/)
+- [RubyGems](https://rubygems.org/) - Explore and install Ruby libraries and tools
+
+## Contributors
+
+This course is brought to you by [Sumit/KodNest]. For contributions, please raise a pull request.
+
+- Lead Instructor: [Sumit](https://github.com/ammyy9908)
+
+
---
+++ week-1/syntax/app.rb
@@ -0,0 +1,224 @@
+
+#comments in ruby
+# this is a comment
+# this is another comment
+# this is a third comment
+#  multiple lines
+=begin
+# this is a comment
+# this is another comment
+=end
+
+# variables in ruby
+# variables are containers for storing data
+# Variable Assignment
+name = "John"
+age = 20
+
+# Variable Re-assignment
+name = "Jane"
+
+# Variable Naming
+# 1. Variable names must start with a letter or an underscore
+# 2. Variable names can only contain letters, numbers, and underscores
+# 3. Variable names are case-sensitive
+# 4. Variable names should use snake_case
+# 5. Variable names should be descriptive
+# 6. Variable names should not start with capital letters
+# 7. Variable names should not contain spaces
+# 8. Variable names should not be reserved words or called keywords
+# 9. Variable names should not start with numbers
+
+# Data Types
+# 1. Numbers
+number = 26
+# 2. Strings
+name = "John"
+# 3. Booleans
+is_true = true
+# 4. Arrays
+names = ["John", "Jane", "Jack"]
+# 5. Hashes
+person = {name: "John", age: 20}
+# 6. Symbols
+:age
+# 7. nil
+isEmpty = nil
+puts isEmpty
+# 8. Range
+numbers = (1..10)
+
+
+# Scope Indicators
+
+# 1. Global Variables
+#examples
+$global_variable = 10
+
+# 2. Instance Variables
+#exaples
+
+@instance_variable = 10
+# 3. Class Variabless
+
+# 4. Local Variables
+#example
+isRainy = true
+puts isRainy
+# 6. Constants
+#example
+PI = 3.14
+puts PI
+
+
+# Operators
+# 1. Arithmetic Operators
+# 2. Assignment Operators
+# 3. Comparison Operators
+# 4. Logical Operators
+# 5. Bitwise Operators
+# 6. Ternary Operators
+# 7. Range Operators
+
+# Arithmetic Operators
+# 1. Addition (+)
+#example
+puts 1 + 1
+# 2. Subtraction (-)
+#example
+puts 1 - 1
+# 3. Multiplication (*)
+#example
+puts 1 * 1
+# 4. Division (/)
+#example
+puts 1 / 1
+# 5. Modulus (%)
+#example
+puts 1 % 1
+# 6. Exponentiation (**)
+#example
+puts 1 ** 1
+# 7. Increment (++)
+#example
+puts ++1
+# 8. Decrement (--)
+#example
+puts --1
+
+# Assignment Operators
+# 1. Equal (=)
+#example
+x = 1
+puts x
+# 2. Plus Equal (+=)
+#example
+x += 1
+# 3. Minus Equal (-=)
+#example
+x -= 1
+# 4. Multiply Equal (*=)
+#example
+x *= 1
+# 5. Divide Equal (/=)
+#example
+x /= 1
+# 6. Modulus Equal (%=)
+#example
+x %= 1
+# 7. Exponentiation Equal (**=)
+#example
+x **= 1
+# 8. Bitwise AND Equal (&=)
+#example
+x &= 1
+# 9. Bitwise OR Equal (|=)
+# example
+x |= 1
+# 10. Bitwise XOR Equal (^=)
+# example
+x ^= 1
+# 11. Bitwise Left Shift Equal (<<=)
+# example
+x <<= 1
+# 12. Bitwise Right Shift Equal (>>=)
+# example
+x >>= 1
+
+
+# Comparison Operators
+# 1. Equal (==)
+# example
+puts 1 == 1
+# 2. Not Equal (!=)
+# example
+puts 1 != 1
+# 3. Greater Than (>)
+# example
+puts 1 > 1
+# 4. Less Than (<)
+# example
+puts 1 < 1
+# 5. Greater Than or Equal To (>=)
+# example
+puts 1 >= 1
+# 6. Less Than or Equal To (<=)
+# example
+puts 1 <= 1
+# 7. Combined Comparison (<=>)
+# example
+puts 1 <=> 1
+# 8. Equality (===)
+# example
+puts 1 === 1
+# 9. Case Equality (===)
+# example
+puts 1 === 1
+
+
+# Logical Operators
+# 1. Logical AND (&&)
+#example
+# 2. Logical OR (||)
+# 3. Logical Nullish Coalescing (??)
+# 4. Logical NOT (!)
+# 5. Logical XOR (^)
+# 6. Logical AND (and
+# 7. Logical OR (or)
+# 8. Logical NOT (not)
+# 9. Logical XOR (xor)
+# 10. Logical AND (&)
+
+# Bitwise Operators
+# 1. Bitwise AND (&)
+# 2. Bitwise OR (|)
+# 3. Bitwise XOR (^)
+# 4. Bitwise NOT (~)
+# 5. Bitwise Left Shift (<<)
+# 6. Bitwise Right Shift (>>)
+# 7. Bitwise Unsigned Right Shift (>>>)
+# 8. Bitwise AND (&.)
+# 9. Bitwise OR (|.)
+# 10. Bitwise XOR (^.)
+# 11. Bitwise NOT (~.)
+# 12. Bitwise Left Shift (<<.)
+
+# Ternary Operators
+# 1. Ternary Operator (? :)
+# 2. Safe Navigation Operator (&.)
+# 3. Conditional Assignment Operator (||=)
+# 4. Conditional Selection Operator (..)
+# 5. Conditional Selection Operator (...)
+# 6. Conditional Selection Operator (?:)
+# 7. Conditional Selection Operator (if..else)
+
+
+
+
+
+
+
+
+
+
+
